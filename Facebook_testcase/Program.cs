1669e66d846a29845c7dbd092a6db64a42344b3d
7Venuspatel <167479825+7Venuspatel@users.noreply.github.com>
2024-04-19 18:19:31 +0000

    i have added my own testcase for entering invalid email address

--- Facebook_testcase/Program.cs
+++ Facebook_testcase/Program.cs
@@ -1,30 +1,74 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System.Linq;
 
-class Program
+namespace FacebookSignup
 {
-    static void Main(string[] args)
+    class Program
     {
-        // Initialize ChromeDriver
-        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+            EnterInValidEmail();
+        }
 
-        // Navigate to Facebook sign-up page
-        driver.Navigate().GoToUrl("https://www.facebook.com/r.php");
+        // User entering valid password
+        static void EnterInValidEmail()
+        {
+            var driver = new EdgeDriver();
+            try
+            {
+                driver.Url = "https://www.facebook.com/r.php";
 
-        // Find the email input field
-        IWebElement emailInput = driver.FindElement(By.Name("reg_email__"));
+                var first_Name = driver.FindElement(By.Name("firstname"));
+                first_Name.SendKeys("John");
 
-        // Enter an invalid email address
-        emailInput.SendKeys("venuspate771gmail.com");
+                var last_Name = driver.FindElement(By.Name("lastname"));
+                last_Name.SendKeys("Doe");
 
-        // Find the sign-up button and click it
-        IWebElement signUpButton = driver.FindElement(By.Name("websubmit"));
-        signUpButton.Click();
+                var mobile_Number = driver.FindElement(By.Name("reg_email__"));
+                mobile_Number.SendKeys("ven123gmail.com");
 
-        // Check if an error message is displayed for invalid email
-       //nEW THINGS
-        // Close the browser
-        Thread.Sleep(TimeSpan.FromSeconds(10));
-        driver.Quit();
+                var password = driver.FindElement(By.Name("reg_passwd__"));
+                password.SendKeys("P@ssw0rd");
+
+                var dob = new SelectElement(driver.FindElement(By.Name("birthday_day")));
+                dob.SelectByValue("1");
+
+                var birthday_Month = new SelectElement(driver.FindElement(By.Name("birthday_month")));
+                birthday_Month.SelectByValue("1");
+
+                var birthday_Year = new SelectElement(driver.FindElement(By.Name("birthday_year")));
+                birthday_Year.SelectByValue("2000");
+
+                var Gender = driver.FindElement(By.CssSelector("input[type='radio'][value='2']"));
+                Gender.Click();
+
+                var signUpButton = driver.FindElement(By.Name("websubmit"));
+                signUpButton.Click();
+
+                var errorMessage = driver.FindElements(By.CssSelector(".uiMessage.error")).Select((x) => x.Text);
+
+                if (errorMessage.Any())
+                {
+                    foreach (var message in errorMessage)
+                    {
+                        Console.WriteLine(message);
+                    }
+                    Console.WriteLine("This test is because it was able to prove.");
+                }
+                else
+                {
+                    Console.WriteLine("This test is failed because it was not able to prove.");
+                }
+                Console.ReadLine();
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
     }
 }
